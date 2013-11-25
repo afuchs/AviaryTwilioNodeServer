82037e0e1f6f4845213d9c72d955fcaeb882ff44
Ari Fuchs <afuchs1@gmail.com>
2013-11-25 14:40:49 -0500

    adds mms support

--- server.js
+++ server.js
@@ -1,41 +1,64 @@
 var express         = require('express'),
     app             = express(),
-    actionLists     = require('./actionLists.json'),
-    Aviary          = require('aviary').Aviary;
+    twilio          = require('twilio'),
+    Aviary          = require('aviary').Aviary,
+    actionLists     = require('./actionlists.json'),
     aviaryClient    = new Aviary(process.env.AVIARY_API_KEY, process.env.AVIARY_API_SECRET);
+
+var config = {
+    authToken: process.env.TWILIO_AUTH_TOKEN,
+    supportsMMS: false
+};
 
 app.use(express.bodyParser());
 
 app.post('/incoming', function(req, res) {
-    var urlPattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
-    var urls = urlPattern.exec(req.body.Body);
 
-    if (urls && urls.length > 0) {
-        aviaryClient.renderAndWait({
-            url: urls[0],
-            actionList: JSON.stringify(actionLists.filters.avenue)
-        }, function(err, renderedUrl) {            
-            res.writeHead(200, {'Content-Type': 'text/xml'});            
-            if (err) {
-                return res.end(generateTwiml('Oops! Something went wrong.'));
-            }             
-            return res.end(generateTwiml('Oooh pretty! ' + renderedUrl));
-        });
+    // Validate Twilio request
+    if (twilio.validateExpressRequest(req, config.authToken)) {
+        var resp = new twilio.TwimlResponse();
+        var imageUrl;
+
+        // Retrieve url from request 
+        if (config.supportsMMS && req.body.NumMedia && req.body.NumMedia > 0) {
+            imageUrl = req.body["MediaUrl" + 1];
+        } else {
+            var urls = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.exec(req.body.Body);
+            imageUrl = (urls && urls.length) ? urls[0] : null;
+        }
+
+        if (imageUrl) {
+            var renderConfig = {
+                url: imageUrl,
+                actionList: JSON.stringify(actionLists.filters.avenue)
+            };
+            
+            // Call the Aviary API to render the image with a filter
+            aviaryClient.renderAndWait(renderConfig, function(err, renderedUrl) {
+                
+                // Generate the appropriate TwiML response
+                if (config.supportsMMS) {
+                    resp.message(function() {
+                        this.body('Oooh pretty!');
+                        this.media(renderedUrl);
+                    });
+                } else {
+                    resp.message('Oooh pretty! ' + renderedUrl);
+                }
+
+                res.type('text/xml');                
+                return res.send(resp.toString());
+            });
+        } else {
+            resp.message('Oops! Try sending an image.');
+            
+            res.type('text/xml');
+            return res.send(resp.toString());
+        }
     } else {
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        return res.end(generateTwiml('Oops! Try sending an image url.'));
+        return res.send('Nice try imposter.');
     }
 });
-
-	function generateTwiml(message) {
-	    return ['<?xml version="1.0" encoding="UTF-8"?>',
-	        '<Response>',
-	            '<Message>',
-	                message,
-	            '</Message>',
-	        '</Response>'
-	        ].join('');
-}
 
 var server = app.listen(process.env.PORT);
 console.log("Listening on port %d in %s mode", server.address().port, app.settings.env);
